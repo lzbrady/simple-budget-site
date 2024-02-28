0048988d13d3874dd7536e08545075ecfce35c51
Luke Brady <lzbrady496@gmail.com>
2024-02-28 13:38:34 -0500

    Updated Gatsby onCreateNode

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,13 +2,29 @@ const { createFilePath } = require("gatsby-source-filesystem");
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
+  // const fileNode = getNode(node.parent);
 
-  if (node.internal.type === `MarkdownRemark`) {
+  if (node.internal.type === `MarkdownRemark` || node.internal.type === `Mdx`) {
+    // const parsedFilePath = path.parse(fileNode.relativePath);
+
+    // let slug;
+    // if (node.frontmatter && node.frontmatter.slug) {
+    //   slug = `/${node.frontmatter.slug}`;
+    // } else {
+    //   if (parsedFilePath.name !== "index" && parsedFilePath.dir !== "") {
+    //     slug = `/${parsedFilePath.dir}/${parsedFilePath.name}`;
+    //   } else if (parsedFilePath.dir === "") {
+    //     slug = `/${parsedFilePath.name}`;
+    //   } else {
+    //     slug = `/${parsedFilePath.dir}`;
+    //   }
+    // }
+
     const value = createFilePath({ node, getNode, basePath: "content/blog" });
     createNodeField({
       name: `slug`,
       node,
-      value,
+      value: `/blog/${value}`,
     });
   }
 };

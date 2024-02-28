32af4013a0e21383d400fe23027340a0300ff771
Luke Brady <lzbrady496@gmail.com>
2024-02-28 12:44:25 -0500

    Added basePath to onCreateNode

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode });
+    const value = createFilePath({ node, getNode, basePath: "content/blog" });
     createNodeField({
       name: `slug`,
       node,
